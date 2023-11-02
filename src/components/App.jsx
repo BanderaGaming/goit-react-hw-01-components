e353d21ee57f28b8eca4e20e240de3baed41a959
BanderaGaming <Glory.To.Ukraine.1648@gmail.com>
2023-11-02 20:15:18 +0100

    Profile ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import userData from '../data/user.json';
+
+import { Profile } from './Profile/Profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile username={userData.username} avatar={userData.avatar} tag={userData.tag} location={userData.location} stats={userData.stats}/>
     </div>
   );
 };

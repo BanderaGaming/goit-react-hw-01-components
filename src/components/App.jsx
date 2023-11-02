9cd42fa8fb1931f2000b82d759e949adc900c013
BanderaGaming <Glory.To.Ukraine.1648@gmail.com>
2023-11-02 20:44:14 +0100

    Stats ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import userData from '../data/user.json';
+import data from '../data/data.json'
 
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
     <div>
       <Profile username={userData.username} avatar={userData.avatar} tag={userData.tag} location={userData.location} stats={userData.stats}/>
+    <Statistics title={"Upload stats"} data={data}/>
     </div>
   );
 };
